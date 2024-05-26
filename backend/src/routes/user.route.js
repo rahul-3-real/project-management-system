2e6f8bcefd5c1ec3930f7b3f8889ac2ffdb7077f
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-05-26 17:14:57 +0530

    Backend - Added Logout Controller and Route with isAuthenticated middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   loginController,
+  logoutController,
   registerController,
 } from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 // Router
 const router = new Router();
@@ -10,5 +12,6 @@ const router = new Router();
 // Routes
 router.route("/register").post(registerController);
 router.route("/login").post(loginController);
+router.route("/logout").post(isAuthenticated, logoutController);
 
 export default router;
